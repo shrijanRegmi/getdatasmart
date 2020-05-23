aac0270f8f94e161bc2d896ed575655bfedc348d
dust <prayatna@bottle.com.np>
2020-05-23 14:08:24 +0545

    success mail to own mail address

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,12 +1,21 @@
 const nodemailer = require("nodemailer");
-
+// sends mail from account to itself. contains the form data in the mail message
 module.exports = function (req, res, next) {
+  const ownMail = "youremailtoreceive@email.com";
+  const pwd = "your_pa$$word";
+  const host = "smtp_host";
   const { name, subject, query, email } = req.body;
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: host,
+    port: 465,
+    secure: true,
     auth: {
-      user: "youremail@address.com",
-      pass: "yourpassword",
+      user: ownMail,
+      pass: pwd,
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
     },
   });
   const message = `<html>
@@ -21,18 +30,19 @@ module.exports = function (req, res, next) {
 </html>
   `;
   const mailOptions = {
-    from: email, // sender address
-    to: "koiralaprayatna9@gmail.com", // list of receivers
+    from: ownMail, // sender address
+    to: ownMail, // list of receivers
     subject: subject, // Subject line
     html: message, // plain text body
   };
 
   transporter.sendMail(mailOptions, function (err, info) {
-    if (err)
+    if (err) {
+      console.log(err);
       res.status(500).send({
         message:
           "Sorry, we couldn't process your request right now. Try again in a little bit!",
       });
-    else next();
+    } else next();
   });
 };
